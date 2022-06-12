e2119d02927cd4ea089f089f23dd0768f141cfa2
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2022-06-12 17:47:06 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "my_demo_rg2" {
   location = "eastus"
   name = "my-demo-rg1"  
 }
