2bcc09462a74fe6226969401943488b620f0aceb
kabin Hamal <kabin.singh.hamal@kyndryl.com>
2022-06-12 15:39:51 -0400

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "my_demo_rg2" {
   location = "eastus"
   name = "my-demo-rg1"  
 }
