12336956629a89a16e266dd7a41bb24659d41b90
kabin Hamal <kabin.singh.hamal@kyndryl.com>
2022-06-12 17:21:32 -0400

    k

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,9 @@ provider "azurerm" {
   features {}
 
   subscription_id = "fd4d574c-3e12-4b08-aa71-d6b3fef00fe2"
+  tenant_id = "f946600a-6e91-4c31-a96f-95977fd26317"
+  client_id = "5852bb57-c05b-494c-ad3b-680a89d3e5ae"
+  client_secret = "kpN8Q~iAHPk8Ea6O_fcOVl47x~Xb_7gTmgYuicQR"
 }
 
 
