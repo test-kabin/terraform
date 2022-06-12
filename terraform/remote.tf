61d7d77263a2bbe0ce1000eea988a35134c5dcdb
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2022-06-12 12:42:05 -0400

    Update remote.tf

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -1,11 +1,12 @@
 
  terraform {
-   backend "remote" {
-     hostname     = "app.terraform.io"
-     organization = "Kyndryl-Github-Actions-Dev1"
- 
-     workspaces {
-       name = "Githunb_Actions_demo"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+   organization = "Kyndryl-Github-Actions-Dev1"
+
+
+    workspaces {
+    name = "Githunb_Actions_demo"
      }
    }
- }
+}
