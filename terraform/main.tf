536ba5039363b5fc557e610fff2049465ae6360f
kabin Hamal <kabin.singh.hamal@kyndryl.com>
2022-06-12 16:58:53 -0400

    k

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,17 @@
-# Configure the Azure provider
-terraform {
-  required_version = "~> 1.2.2"
+erraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=3.0.0"
     }
   }
 }
 
-
 # Configure the Microsoft Azure Provider
-provider "azurerm" {    
-   
-#skip provider rego because we are using service principal 
-#with limited access to azure
-   skip_provider_registration = "true"
+provider "azurerm" {
+  features {}
 
-  clientId = "5852bb57-c05b-494c-ad3b-680a89d3e5ae",
-  clientSecret = "kpN8Q~iAHPk8Ea6O_fcOVl47x~Xb_7gTmgYuicQR",
-  subscriptionId = "fd4d574c-3e12-4b08-aa71-d6b3fef00fe2",
-  tenantId = "f946600a-6e91-4c31-a96f-95977fd26317",
-   
-   features {}
-   
+  subscription_id = "fd4d574c-3e12-4b08-aa71-d6b3fef00fe2"
 }
 
 
